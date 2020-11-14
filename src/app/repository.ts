5f97521483e22f6852916fd563c443f64c06e6c7
Steve99-coder <stevenderitu99@gmail.com>
2020-11-14 21:59:47 +0300

    Add the class for  the user and repository

---
+++ src/app/repository.ts
@@ -0,0 +1,10 @@
+export class Repository {
+    constructor(public name: string,
+        public html_url: string,
+        public description: string,
+        public created_at: Date,
+        public forks: number,
+        public watchers_count: number,
+        public language: string) {
+    }
+}
---
+++ src/app/user.ts
@@ -0,0 +1,18 @@
+export class User {
+    static result: any;
+    constructor(
+        public url: string,
+        public login: string,
+        public html_url: string,
+        public location: string,
+        public public_repos: number,
+        public followers: number,
+        public following: number,
+        public avatar_url: string,
+        public bio: string,
+        public created_at: Date
+
+        ) {
+
+    }
+}
