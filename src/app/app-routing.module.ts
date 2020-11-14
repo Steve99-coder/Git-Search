6194faec0b2a82c0e688529a5e209cf72a3e2c0c
Steve99-coder <stevenderitu99@gmail.com>
2020-11-14 23:36:35 +0300

    Import the user component in the app module ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { AboutComponent } from './about/about.component';
+import { UserComponent } from './user/user.component';
 
 const routes: Routes = [];
 
