93c6fd0aa57333140b7918a0066c6e0f99e57efd
Steve99-coder <stevenderitu99@gmail.com>
2020-11-14 23:37:52 +0300

    Add the routes for the user and the about

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,10 @@ import { NotFoundComponent } from './not-found/not-found.component';
 import { AboutComponent } from './about/about.component';
 import { UserComponent } from './user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'user', component: UserComponent},
+  { path: 'about', component: AboutComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
