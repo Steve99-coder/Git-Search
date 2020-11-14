914bb07c6cd5d53cdf9a085371f87d400b0d5843
Steve99-coder <stevenderitu99@gmail.com>
2020-11-14 23:35:25 +0300

    Link the not found component and about component to the app route module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [];
 
