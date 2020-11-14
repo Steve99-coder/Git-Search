a8c83926ced3f4a5ca1773c96e796a89619568a4
Steve99-coder <stevenderitu99@gmail.com>
2020-11-14 22:53:06 +0300

    Add the functionality to the user form ts

--- src/app/user-form/user-form.component.ts
+++ src/app/user-form/user-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-user-form',
@@ -6,7 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-form.component.css']
 })
 export class UserFormComponent implements OnInit {
-
+  searchName:string
+  @Output() searchUser=new EventEmitter<any>();
+   
+  search(){ 
+    this.searchUser.emit(this.searchName)
+    this.searchName = ""
+  }  
   constructor() { }
 
   ngOnInit(): void {
